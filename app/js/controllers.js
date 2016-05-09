d99b510547445512a8f0cfe522c4a377cbe99876
Pato <pevargas@valdosta.edu>
2016-05-09 00:46:47 -0400

    5 dependency injection

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,13 +6,12 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
-});
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+
+    $scope.orderProp = 'age';
+
+  }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,15 +1,22 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
-describe('PhoneListCtrl', function(){
+describe('PhoneCat controllers', function() {
 
+describe('PhoneListCtrl', function(){
+  var scope, ctrl, $httpBackend;
+
+  // Load our app module definition before each test.
   beforeEach(module('phonecatApp'));
 
-  it('should create "phones" model with 3 phones', inject(function($controller) {
-    var scope = {},
-        ctrl = $controller('PhoneListCtrl', {$scope:scope});
+  // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+  // This allows us to inject a service but then attach it to a variable
+  // with the same name as the service in order to avoid a name conflict.
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('phones/phones.json').
+        respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-    expect(scope.phones.length).toBe(3);
+    scope = $rootScope.$new();
+    ctrl = $controller('PhoneListCtrl', {$scope: scope});
   }));
-
-});
